84336223dc05d0d126fc878a71ba8b015a03df2f
Atharva Raykar <raykar.ath@gmail.com>
2019-11-18 23:33:31 +0530

    Add committing

--- tvs.cpp
+++ tvs.cpp
@@ -35,6 +35,8 @@ private:
     // int next_id;
     // int prev_id;
     string commit_msg;
+    vector<string> added_files;
+    int num_added;
 
 public:
     // Commit *next, *prev;
@@ -50,7 +52,8 @@ public:
         return commit_msg;
     }
     void make_commit(string msg);
-    void store_files(string *added_files);
+    void add_file(string filename);
+    void store_files();
     void display_commit_data();
     void save(ofstream &ofile);
     void open(ifstream &ifile);
@@ -64,24 +67,50 @@ Commit::Commit() : info(A_info())
         getline(ifile, commit_msg);
     } else {
         ifile.close();
+        commit_id = "-1";
         ofstream ofile("./.tvs/branch_info");
         ofile << "0" << endl;
         ofile << "init" << endl;
         ofile.close();
-        cout << "No commits yet" << endl;
+        num_added = 0;
+        cout << "No commits yet. Making first commit." << endl;
     }
     ifile.close();
 }
 
 void Commit::make_commit(string msg)
 {
-    int id = stoi(commit_id);
-    commit_id = to_string(++id);
-    commit_msg = msg;
-    remove("./.tvs/branch_info");
-    ofstream ofile("./.tvs/branch_info");
-    ofile << commit_id << endl;
-    ofile << commit_msg << endl;
+    if(!added_files.empty()) {
+        int id = stoi(commit_id);
+        commit_id = to_string(++id);
+        commit_msg = msg;
+        remove("./.tvs/added_files");
+        remove("./.tvs/branch_info");
+        ofstream ofile("./.tvs/branch_info");
+        ofile << commit_id << endl;
+        ofile << commit_msg << endl;
+        fs::path dst("./.tvs/" + commit_id + "/");
+        fs::create_directory("./.tvs/" + commit_id);
+        for(auto file : added_files) {
+            fs::copy("./" + file, dst);
+        }
+    } else {
+        cout << "Please add files to commit." << endl;
+    }
+}
+
+void Commit::add_file(string filename)
+{
+    added_files.push_back(filename);
+}
+
+void Commit::store_files()
+{
+    ofstream ofile("./.tvs/added_files");
+    for(auto it : added_files) {
+        ofile << it << endl;
+    }
+    ofile.close();
 }
 
 // Commit::~Commit() : info(A_info())
@@ -119,6 +148,8 @@ int main()
 {
     tvsinit();
     Commit c;
+    c.add_file("tvs.cpp");
+    c.add_file("DOCS.md");
     c.make_commit("henlo");
     cout << c.get_commit_id();
     // master_branch.commit_log(2);
